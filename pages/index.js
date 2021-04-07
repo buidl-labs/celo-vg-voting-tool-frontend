08627f20563c2a2824d212a344c0dedf314acf48
Manan Gouhari <gouhari21@hotmail.com>
2021-04-07 19:04:13 +0530

    Setup ChakraUI

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
+import { Container } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Celo Voting Tool</title>
       </Head>
       <div>Celo Voting tool</div>
-    </div>
+    </Container>
   );
 }
